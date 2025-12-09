8ba8dda00a6fc76d349cc229ad8cedf1b8170ac3
Teodor Glisic <teodor.glisic@students.fhnw.ch>
2025-12-09 20:00:04 +0100

    AB#52 Suchmethode mit GET auf /api/books/search mit query 'keyword' erstellt

--- src/main/java/fhnw/aalm_catalog/BookRestController.java
+++ src/main/java/fhnw/aalm_catalog/BookRestController.java
@@ -2,8 +2,10 @@ package fhnw.aalm_catalog;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -20,5 +22,34 @@ public class BookRestController {
     @GetMapping(path = "/books", produces = "application/json")
     public List<Book> allBooks() {
         return this.bookRepository.findAll();
+    }
+
+    @GetMapping(path = "/books/search", produces = "application/json")
+    public List<Book> searchBooks(@RequestParam(name = "keyword") String query) {
+        String[] querySplit = query.toUpperCase().split(",");
+        List<Book> results = new ArrayList<>();
+
+        boolean check = true;
+
+        for (Book b: this.bookRepository.findAll()) {
+
+            boolean matchesAll = true;
+
+            for (String keyword: querySplit) {
+                boolean matchesOne =
+                        b.getAuthor().toUpperCase().equals(keyword) ||
+                        b.getTitle().toUpperCase().equals(keyword) ||
+                        b.getDescription().toUpperCase().equals(keyword) ||
+                        b.getIsbn().equals(keyword);
+
+                if (!matchesOne) {
+                    matchesAll = false;
+                    break;
+                }
+            }
+
+            if (matchesAll) results.add(b);
+        }
+        return results;
     }
 }
