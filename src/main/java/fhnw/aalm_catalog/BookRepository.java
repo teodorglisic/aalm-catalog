6f1f896730669de8f88e30755461529b655ee1a1
Teodor Glisic <teodor.glisic@students.fhnw.ch>
2025-12-09 19:24:28 +0100

    AB#51 BookRepository erstellt

---
+++ src/main/java/fhnw/aalm_catalog/BookRepository.java
@@ -0,0 +1,8 @@
+package fhnw.aalm_catalog;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, String> {
+}
