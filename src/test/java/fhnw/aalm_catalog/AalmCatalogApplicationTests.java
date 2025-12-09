bf0649aefadf76d0aeed72a9e2eaadc185fafc1a
Teodor Glisic <teodor.glisic@students.fhnw.ch>
2025-12-09 19:05:38 +0100

    AB#50 Spring boot catalog aufgesetzt

---
+++ src/test/java/fhnw/aalm_catalog/AalmCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package fhnw.aalm_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AalmCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
