51ae9c0e144c9fc2ca81f8d3834472fa0af5c7a8
Teodor Glisic <teodor.glisic@students.fhnw.ch>
2025-12-09 19:12:31 +0100

    AB#50 Book entity erstellt

---
+++ src/main/java/fhnw/aalm_catalog/Book.java
@@ -0,0 +1,48 @@
+package fhnw.aalm_catalog;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    private String isbn;
+    private String title;
+    private String description;
+    private String author;
+
+
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
